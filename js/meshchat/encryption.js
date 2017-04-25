ac34a3d8eb9b23b9933b3a0495d35a53f7b20ea8
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-04-25 06:33:23 -0400

    begin implementing the meshchat

---
+++ js/meshchat/encryption.js
@@ -0,0 +1,15 @@
+import * as NaCl from 'tweetnacl';
+
+// @param [String] msg - payload to be encrypted
+// @param [String|PublicKey] for_public_key - the public key of the intended recipient
+// @param [String|PrivateKey] with_private_key - (usually) our private key
+export function encrypt(msg, forPublicKey, withPrivateKey) {
+
+}
+
+// @param [String] msg - payload to be encrypted
+// @param [String|PublicKey] from_public_key - the public key of the intended recipient
+// @param [String|PrivateKey] with_private_key - (usually) our private key
+export function decrypt(msg, fromPublicKey, withPrivateKey) {
+
+}
