2bfd38d199737bc34bc04e0ef98fa76fdb02ba88
NullVoxPopuli <LPSego3+dev@gmail.com>
2017-04-29 17:34:18 -0400

    fixed styling of the footer

--- js/components/index.jsx
+++ js/components/index.jsx
@@ -15,25 +15,25 @@ export default class Index extends Component {
     return (
       <div id='index-page-inner'>
         <div id='wrap'>
-        <h3 className='hidden-sm-down text-center'>Welcome to tanqueRéact!</h3>
-        <h4 className='hidden-md-up text-center'>Welcome to tanqueRéact!</h4>
-        <br />
-        <hr style={{ width: '50%' }} />
-        <p className='text-center mx-auto' style={{ fontSize: '1.3em', maxWidth: '450px' }}>
-          The <em>open source</em> p2p encrypted chat client that operates over
-          <em>open source</em> mesh nodes on free-tier cloud services. <br />
-          Deploy your own 'members-only' chat in minutes!
-        </p>
+          <h3 className='hidden-sm-down text-center'>Welcome to tanqueRéact!</h3>
+          <h4 className='hidden-md-up text-center'>Welcome to tanqueRéact!</h4>
+          <br />
+          <hr style={{ width: '50%' }} />
+          <p className='text-center mx-auto' style={{ fontSize: '1.3em', maxWidth: '450px' }}>
+            The <em>open source</em> p2p encrypted chat client that operates over
+            <em>open source</em> mesh nodes on free-tier cloud services. <br />
+            Deploy your own 'members-only' chat in minutes!
+          </p>
 
-        <div className='mx-auto mt-5' style={{ display: 'table' }}>
-          <Button
-            size='lg'
-            color='success'
-            onClick={toChat}>
-            Begin Chatting
-          </Button>
+          <div className='mx-auto mt-5' style={{ display: 'table' }}>
+            <Button
+              size='lg'
+              color='success'
+              onClick={toChat}>
+              Begin Chatting
+            </Button>
+          </div>
         </div>
-      </div>
 
         <footer className='w-100 bg-faded p-3'>
           <div className='container'>
