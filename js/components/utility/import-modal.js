a1408a78fee305d5fb13b6eb267c7756dd512155
Preston Sego <LPSego3+dev@gmail.com>
2017-09-28 07:09:49 -0400

    add qr codes

--- js/components/utility/import-modal.js
+++ js/components/utility/import-modal.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
-import { FormGroup, Input } from 'reactstrap';
+import { FormGroup, Input, Button } from 'reactstrap';
 import { connect } from 'react-redux';
+// import * as Instascan from 'instascan';
+const Instascan = require('instascan');
+
 
 import { mutCreator } from 'react-state-helpers';
 import FileChooser from 'components/file-chooser';
 import SimpleModal from 'components/-components/simple-modal';
 
-import { toastSuccess } from 'utility/toast';
+import { toastSuccess, toastError } from 'utility/toast';
 
 import { data } from 'js/actions';
 import { isUserIdentityValid } from 'js/actions/data/users';
@@ -19,7 +22,13 @@ const placeholder = `{
   "uid": "AUniqueIdentifier"
 }`;
 
-class ImportModal extends Component {
+const mapStateToProps = () => ({});
+const mapDispatchToProps = dispatch => ({
+  importUser: bindActionCreators(data.users.importUser, dispatch)
+});
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class ImportModal extends Component {
   static propTypes = {
     importUser: PropTypes.func.isRequired,
     tagName: PropTypes.string
@@ -29,6 +38,7 @@ class ImportModal extends Component {
     super(props);
 
     this.state = {
+      scanning: false,
       identity: null,
       importDisabled: true,
       Tag: props.tagName || 'li'
@@ -38,7 +48,33 @@ class ImportModal extends Component {
     this.didClickImport = this.didClickImport.bind(this);
     this.didSelectFile = this.didSelectFile.bind(this);
     this.identityChanged = this.identityChanged.bind(this);
+    this.didClickScanQRCode = this.didClickScanQRCode.bind(this);
   }
+  //
+  // componentDidMount() {
+  //   let scanner = new Instascan.Scanner({
+  //     video: document.getElementById('preview'),
+  //     mirror: false,
+  //     continuous: true
+  //   });
+  //
+  //   scanner.addListener('scan', content => {
+  //     alert(content);
+  //   });
+  //
+  //   Instascan.Camera.getCameras()
+  //     .then(cameras => {
+  //       if (cameras.length > 0) {
+  //         scanner.start(cameras[0]);
+  //       } else {
+  //         toastError('No Cameras Found');
+  //       }
+  //     }).catch(e => {
+  //       toastError(e);
+  //     });
+  //
+  // }
+
 
   didClickImport() {
     const identityToImport = this.state.identity;
@@ -69,10 +105,39 @@ class ImportModal extends Component {
     this.setState({ identity: prettyJson, importDisabled });
   }
 
+  async didClickScanQRCode() {
+    this.setState({ scanning: true });
+
+    let scanner = new Instascan.Scanner({
+      video: document.getElementById('preview'),
+      mirror: false,
+      continuous: true
+    });
+
+    scanner.addListener('scan', content => {
+      this.setState({ identity: content });
+    });
+
+
+    try {
+      const cameras = await Instascan.Camera.getCameras();
+
+      if (cameras.length > 0) {
+        await scanner.start(cameras[0]);
+      } else {
+        toastError('No Cameras Found');
+        this.setState({ scanning: false })
+      }
+    } catch(e) {
+      toastError(e.message);
+      this.setState({ scanning: false })
+    }
+  }
+
   // TODO: add validation
   render() {
-    const { identity, importDisabled, Tag } = this.state;
-    const { didClickImport, didSelectFile, identityChanged } = this;
+    const { identity, importDisabled, Tag, scanning } = this.state;
+    const { didClickImport, didSelectFile, identityChanged, didClickScanQRCode } = this;
 
     return (
       <Tag>
@@ -83,36 +148,40 @@ class ImportModal extends Component {
           confirmText='Import'
           confirmDisabled={importDisabled}
           additionalFooterButtons={
-            <FileChooser
-              onChange={didSelectFile}
-              buttonText={'Select Identity File'} />}>
-          <h4>Paste Identity File Here</h4>
+            <span className='d-flex flex-row'>
+              <FileChooser
+                onChange={didSelectFile}
+                buttonText={'Select File'} />
+              <Button onClick={didClickScanQRCode}>
+                <i className='fa fa-qrcode'></i>
+              </Button>
+            </span>
 
-          <FormGroup>
-            <Input
-              type='textarea'
-              rows={5}
-              onChange={identityChanged}
-              value={identity || ''}
-              placeholder={placeholder} />
-          </FormGroup>
+            }>
 
-          <p>
-            This should contain the keys&nbsp;
-            <em><strong>alias</strong></em>,&nbsp;
-            <em><strong>publickey</strong></em>, and&nbsp;
-            <em><strong>uid</strong></em>
-          </p>
+          { scanning && <video id="preview" autoPlay="autoplay"></video> }
+          { !scanning && <span>
+            <h4>Paste Identity File Here</h4>
+
+            <FormGroup>
+              <Input
+                type='textarea'
+                rows={5}
+                onChange={identityChanged}
+                value={identity || ''}
+                placeholder={placeholder} />
+            </FormGroup>
+
+            <p>
+              This should contain the keys&nbsp;
+              <em><strong>alias</strong></em>,&nbsp;
+              <em><strong>publickey</strong></em>, and&nbsp;
+              <em><strong>uid</strong></em>
+            </p>
+          </span>}
 
         </SimpleModal>
       </Tag>
     );
   }
 }
-
-const mapStateToProps = () => ({});
-const mapDispatchToProps = dispatch => ({
-  importUser: bindActionCreators(data.users.importUser, dispatch)
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ImportModal);
