6c2f68184eef8b9ab94202a7e6dbb3e8decac946
Preston Sego <LPSego3+dev@gmail.com>
2017-10-22 17:17:19 -0400

    move the rsa stuff to the master branch

--- scripts/ci/travis.sh
+++ scripts/ci/travis.sh
@@ -1,7 +1,19 @@
 if [ "$TRAVIS_BRANCH" == "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
-   echo "Deploying the 'master' branch..."
-   ./scripts/ci/deploy.sh;
+  # We only care about publishing on master...
+  echo "Preparing Deploy"
+
+  cp ./scripts/ci/travis-id_rsa.enc ./
+  openssl aes-256-cbc -K $encrypted_bee6fd901ba1_key -iv $encrypted_bee6fd901ba1_iv -in travis-id_rsa.enc -out travis-id_rsa -d
+  chmod 600 travis-id_rsa
+  cp travis-id_rsa $HOME/.ssh/id_rsa
+
+  echo "Deploying the 'master' branch..."
+
+  ./scripts/ci/deploy.sh;
+
+  echo "Deployed!"
  else
-   echo "Building..."
-   ./scripts/ci/transpile.sh;
+  echo "Building..."
+
+  ./scripts/ci/transpile.sh;
  fi
